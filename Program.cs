58de10cf084801a892df66ada11367a0d30a99c9
Sauljr144 <saulsuazojr06@gmail.com>
2022-10-19 20:55:33 -0700

    play again loop added.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
 ﻿//On this challange I am adding two numbers that the user will input.
 
+
+string playAgain = "Y";
+
 //This displays to the user to prompt an input.
 Console.WriteLine("We will add 2 numbers.");
-Console.WriteLine("Please type a number.");
+
+while(playAgain != "N")
+{
+
+    Console.WriteLine("Please type a number.");
 
 
-//This stores and converts
-int number1 = Convert.ToInt32(Console.ReadLine());
+    //This stores and converts
+    int number1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Please type another number.");
+    Console.WriteLine("Please type another number.");
 
-int number2 = Convert.ToInt32(Console.ReadLine());
+    int number2 = Convert.ToInt32(Console.ReadLine());
 
-//This adds the two numbers using interpolation.
-Console.WriteLine($"Your answer is {number1 + number2}.");
+    //This adds the two numbers using interpolation.
+    Console.WriteLine($"Your answer is {number1 + number2}.");
+
+    Console.WriteLine("Do you want to play again? Y/N");
+    playAgain = Console.ReadLine().ToUpper();
+
+}
+
+Console.WriteLine("Bye!");

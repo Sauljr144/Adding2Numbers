f37446cbad4ab8de0ca2c6a2b9b3d6bfd2d91843
Sauljr144 <saulsuazojr06@gmail.com>
2022-10-24 20:52:09 -0700

    data validation added.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,53 @@
 ﻿//On this challange I am adding two numbers that the user will input.
 
 
+int myNum;
+int myNum2;
 string playAgain = "Y";
+bool ValidNum;
+bool ValidNum2;
 
 //This displays to the user to prompt an input.
 Console.WriteLine("We will add 2 numbers.");
 
 while(playAgain != "N")
 {
-
     Console.WriteLine("Please type a number.");
 
+    ValidNum = int.TryParse(Console.ReadLine(), out myNum);
+    
+    if(!ValidNum)
+    {
+        Console.WriteLine("Please enter a valid number.");
+    }
 
-    //This stores and converts
-    int number1 = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine("Please type another number.");
+    else
+    {
+        //This stores and converts
+        // int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please type another number.");
+        ValidNum2 = int.TryParse(Console.ReadLine(), out myNum2);
 
-    int number2 = Convert.ToInt32(Console.ReadLine());
+        if(!ValidNum2)
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+        
 
-    //This adds the two numbers using interpolation.
-    Console.WriteLine($"Your answer is {number1 + number2}.");
+        // int number2 = Convert.ToInt32(Console.ReadLine());
+        else
+        {
+             //This adds the two numbers using interpolation.
+            Console.WriteLine($"Your answer is {myNum + myNum2}.");
 
-    Console.WriteLine("Do you want to play again? Y/N");
-    playAgain = Console.ReadLine().ToUpper();
+            Console.WriteLine("Do you want to play again? Y/N");
+            playAgain = Console.ReadLine().ToUpper();
+        }
+       
 
+    }
+    
 }
 
 Console.WriteLine("Bye!");
